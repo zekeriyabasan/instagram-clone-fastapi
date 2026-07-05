d86bd055508ffc7a52f76c973ef3e4cb8bfb8522
zekeriyabasan <zekeriyabasan@gmail.com>
2026-07-05 12:35:09 +0300

    created db

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from db import models
+from db.database import engine
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+models.Base.metadata.create_all(engine)
