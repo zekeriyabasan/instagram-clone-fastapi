459e6d920808a45b8b4f7694aed880cfcc9d69f3
zekeriyabasan <zekeriyabasan@gmail.com>
2026-07-11 15:00:23 +0300

    hashing

--- db/db_user.py
+++ db/db_user.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm.session import Session
 from db.models import DbUser
 from routers.schemas import UserBase
-
+from db.hashing import Hash
 
 def create_an_user(db: Session, user: UserBase):
     db_user = DbUser(
         username=user.username,
         email=user.email,
-        password=user.password,
+        password=Hash.bcrypt(user.password),
     )
     db.add(db_user)
     db.commit()
